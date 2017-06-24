1a0e90f5d11f85ed8cbb21c82192de02c7da9186
Вадим <vadim.belyy.02@mail.ru>
2017-06-24 11:55:53 +0300

    -

--- RFID/RFID.ino
+++ RFID/RFID.ino
@@ -1,28 +1,49 @@
-#include <SPI.h>
-#include <MFRC522.h>
+#include
+#include
 
-const int RST_PIN = 9; // пин RST
-const int SS_PIN = 10; // пин SDA (SS)
+#define RST_PIN  9  //
+#define SS_PIN  10  //
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); // создаём объект MFRC522
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 
 void setup() {
-  Serial.begin(9600); // инициализация послед. порта
-  SPI.begin(); // инициализация шины SPI
-  mfrc522.PCD_Init(); // инициализация считывателя RC522
+ Serial.begin(9600);  // Инициализируем монитор последовательного порта
+ while (!Serial);  // Ничего не делаем пока он не открыт (для Arduino на чипе ATMEGA32U4)
+ SPI.begin();   // Инициализируем SPI шину
+ mfrc522.PCD_Init();  // Инициализируем RFID модуль
+ ShowReaderDetails(); // Выводим данные о модуле MFRC522
+ Serial.println(F("Scan PICC to see UID, type, and data blocks..."));
 }
 
 void loop() {
-  // Ожидание прикладывания новой RFID-метки:
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return; // выход, если не приложена новая карта
-  }
+ // Ищем новую карту
+ if ( ! mfrc522.PICC_IsNewCardPresent()) {
+  return;
+ }
 
-  // Считываем серийный номер:
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return; // выход, если невозможно считать сер. номер
-  }
+ // Выбираем одну из карт
+ if ( ! mfrc522.PICC_ReadCardSerial()) {
+  return;
+ }
 
-  // Вывод дампа в послед. порт:
-  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+ // Выводим данные с карты
+ mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+}
+
+void ShowReaderDetails() {
+ // Получаем номер версии модуля
+ byte v = mfrc522.PCD_ReadRegister(mfrc522.VersionReg);
+ Serial.print(F("MFRC522 Software Version: 0x"));
+ Serial.print(v, HEX);
+ if (v == 0x91)
+  Serial.print(F(" = v1.0"));
+ else if (v == 0x92)
+  Serial.print(F(" = v2.0"));
+ else
+  Serial.print(F(" (unknown)"));
+ Serial.println("");
+ // Когда получаем 0x00 или 0xFF, передача данных нарушена
+ if ((v == 0x00) || (v == 0xFF)) {
+  Serial.println(F("WARNING: Communication failure, is the MFRC522 properly connected?"));
+ }
 }
